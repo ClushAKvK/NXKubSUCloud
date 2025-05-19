9d2911b5df8ba7d42296bf22471d0192e7b3b6f3
Aleksandr Shirko <shirko.02@mail.ru>
2025-05-19 17:56:45 +0000

    Добавлен метод проверки формата файла

---
+++ version.php
@@ -0,0 +1,18 @@
+<?php 
+$OC_Version = array(29,0,0,19);
+$OC_VersionString = '29.0.0';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array (
+  'nextcloud' => 
+  array (
+    '28.0' => true,
+    '29.0' => true,
+  ),
+  'owncloud' => 
+  array (
+    '10.13' => true,
+  ),
+);
+$OC_Build = '2024-04-23T15:11:45+00:00 744e0ddb1e9117a1d912ff29dfdac62d1e1dc72e';
+$vendor = 'nextcloud';
